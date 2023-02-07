47ae6e710039f60777aeef888e9b06a1d2e4877e
Elias <eliasc20022002@gmail.com>
2023-02-07 15:27:19 -0600

    Initial Commit with Angular Project

---
+++ ejemplo/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CounterComponent } from './counter/counter.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CounterComponent,
+    LoginFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
